17c0de8f5d727110cb99277ecb8834cabecf051b
NISHIDA Shohei <52852562+NISHIDA-Shohei@users.noreply.github.com>
2023-12-27 14:37:44 +0900

    Update main.py
    ツイート文字数上限を4000字に

--- main.py
+++ main.py
@@ -202,7 +202,7 @@ def generate_tweet(retry_count, result):
     result = result.strip('"') 
     character_count = int(parse_tweet(result).weightedLength)
     
-    if 1 <= character_count <= 1000: 
+    if 1 <= character_count <= 4000: 
         try:
             if image_result:
                 # Download image from URL
